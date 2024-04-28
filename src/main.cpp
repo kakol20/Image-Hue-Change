1c94c4f32a9b11e68d0ee045a05095cdd76f551f
Adrian Aquino-Neary <adrianaquino.aquino33@gmail.com>
2024-04-28 21:39:43 +0100

    Switch to using vector

--- src/main.cpp
+++ src/main.cpp
@@ -6,21 +6,28 @@
 int main(int argc, char* argv[]) {
   std::cout << "Hello World\n";
 
-  double tempArr[] = {
+  Matrix lhs({
     1., 2., 1.,
     0., 1., 0.,
-    2., 3., 4.
-  };
-
-  Matrix lhs(tempArr, 3, 3);
+    2., 3., 4.}, 3, 3);
+  Matrix rhs({
+    2, 5,
+    6, 7,
+    1, 8}, 2, 3);
   Matrix lhsInv = lhs;
   lhsInv.Invert3x3();
+
+  Matrix mult = lhs * rhs;
+  Matrix multInv = lhsInv * mult;
 
   //tempArr = { 1 };
 
   //std::cout <<
   std::cout << lhs.Debug();
+  std::cout << rhs.Debug();
   std::cout << lhsInv.Debug();
+  std::cout << mult.Debug();
+  std::cout << multInv.Debug();
 
   std::cout << "Press enter to exit...\n";
   std::cin.ignore();
